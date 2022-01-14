9c81dfef53c606bfe5a99484d6d4fa11124e83c4
airrvteam <97501782+airrvteam@users.noreply.github.com>
2022-01-14 00:05:53 -0600

    My first update 01102022
    Trial. Updates for part 1: Branding

--- src/marketplace-custom-config.js
+++ src/marketplace-custom-config.js
@@ -113,6 +113,9 @@ export const filters = [
         { key: 'restorative', label: 'Restorative' },
         { key: 'vinyasa', label: 'Vinyasa' },
         { key: 'yin', label: 'Yin' },
+        { key: 'petsathome', label: 'Pets at home' },
+        { key: 'sharedcameras', label: 'Shared Cameras' },
+        { key: 'covid19vaccined', label: 'COVID-19 Vaccined' },
       ],
     },
   },
@@ -134,6 +137,30 @@ export const filters = [
       ],
     },
   },
+//***************************added for testing****************************zwq
+{
+    id: 'amenitieszwq',
+    label: 'Amenities',
+    type: 'SelectMultipleFilter',
+    group: 'secondary',
+    queryParamNames: ['pub_amenitieszwq'],
+    config: {
+      // Optional modes: 'has_all', 'has_any'
+      // https://www.sharetribe.com/api-reference/marketplace.html#extended-data-filtering
+      searchMode: 'has_all',
+
+      // "key" is the option you see in Flex Console.
+      // "label" is set here for this web app's UI only.
+      // Note: label is not added through the translation files
+      // to make filter customizations a bit easier.
+      options: [
+        { key: 'petathome', label: 'Pet at home' },
+        { key: 'sharedcameras', label: 'Shared Cameras' },
+        { key: 'covid19vaccined', label: 'COVID-19 Vaccined' },
+      ],
+    },
+  },
+//*****************************end of my test***********************zwq
 ];
 
 export const sortConfig = {
